b50323f30fc37662679427b436dc0ca64e80f302
Kovalev Aleksey <akovalev@ics.perm.ru>
2019-10-30 23:56:18 +0500

    Create solution with test implementation of a hierarchy of interfaces for work

---
+++ TestInterfaceHierarchy/TestInterfaceHierarchy/Main.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestInterfaceHierarchy
+{
+    using ContentClass;
+    using Factory;
+    using Interface;
+
+    public class Main
+    {
+        public void Test()
+        {
+            IAgencyContentFactory<TreeList> factory = new AgencyTreeListFactory();//Resolve DI
+            var treeList = factory.GetSpContent();
+        }
+    }
+}
